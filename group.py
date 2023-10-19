9ce6a9ab11a5f50520baf80d37c519dd65260256
Husheng Li <zczqhl2@ucl.ac.uk>
2023-10-19 11:15:31 +0100

    Q9

--- group.py
+++ group.py
@@ -3,3 +3,30 @@
 # Your code to go here...
 
 my_group = 123
+
+# class person:
+#     def __init__(self, name, age, job, **connection):
+#         self.name = name
+#         self.age = age  # Note the default argument, occupants start empty
+#         self.job = job
+#         self.connection = connection
+
+# Jill = person.__init__("Jill", 26, "biologist", "Zalika":"friend", "John":"partner")
+# Zalika = person.__init__("Zalika", 28, "artist", "Jill" friend)
+# John = person.__init__
+# Nash = person.__init__
+
+
+# -  is 27, a writer, and Jill's partner.
+# -  is 34, a chef, John's cousin and Zalika's landlord.
+
+- Jill is 26, a biologist and she is Zalika's friend and John's partner.
+- Zalika is 28, an artist, and Jill's friend
+- John is 27, a writer, and Jill's partner.
+- Nash is 34, a chef, John's cousin and Zalika's landlord.
+
+age = {"Jill":26, "Zalika":28, "John":27, "Nash":34}
+
+job = {"Jill":"biologist", "Zalika":"artist", "John":"writer", "Nash":"chef"}
+
+connection = {}
